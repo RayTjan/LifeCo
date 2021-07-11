ac060767af99b24f4dcface6708116b9735d54c6
Bryan <bryanmuliawan@yahoo.co.id>
2021-07-11 14:35:23 +0700

    Add socs layout

--- lifeco/app/src/main/java/com/example/LifeCo/activities/socsActivity.java
+++ lifeco/app/src/main/java/com/example/LifeCo/activities/socsActivity.java
@@ -1,4 +1,16 @@
 package com.example.LifeCo.activities;
 
-public class socsActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.lifeco.R;
+
+public class socsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_socs);
+    }
 }
